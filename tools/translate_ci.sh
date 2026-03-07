30f0f40f0c21ef608fa7aa30dfc89c91637c4593
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-03-07 14:00:48 +0000

    fix ci translation

--- tools/translate_ci.sh
+++ tools/translate_ci.sh
@@ -8,11 +8,15 @@ git config --local core.hooksPath /notexistent
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
 
+tsx tools/translate.ts
+
 export TZ=
 git diff --exit-code i18n || {
+  git stash
 	git pull
+  git stash pop
 	git add i18n
-	git commit -m "sync"
+	git commit -m "translate ru -> en"
 	git push
 }
 
